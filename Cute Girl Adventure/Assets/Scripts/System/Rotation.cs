08bb1319786ca89b6651f281dcfb51dc8a1b3187
libearis <livensiswoyo@gmail.com>
2023-02-03 11:01:44 +0700

    Level 7 Update

--- Cute Girl Adventure/Assets/Scripts/System/Rotation.cs
+++ Cute Girl Adventure/Assets/Scripts/System/Rotation.cs
@@ -6,14 +6,14 @@ using TMPro;
 public class Rotation : MonoBehaviour
 {
     public float rotationSpeed;
-    [HideInInspector] public bool isSlowingDown, isStopped;
+    public bool isSlowingDown, isStopped;
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
         if (isSlowingDown && rotationSpeed >= 0)
         {
             rotationSpeed -= Random.Range(100, rotationSpeed) * Time.deltaTime;
-        }
+        } 
         if(rotationSpeed < 0)
         {
             isSlowingDown = false;
